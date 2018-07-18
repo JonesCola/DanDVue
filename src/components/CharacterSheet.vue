25181706a3478c920f38fa3b9dfbb188cadd9c9b
Aaron Baum <jonescola7@gmail.com>
2018-07-18 17:17:21 -0600

    character sheet is binding

---
+++ src/components/CharacterSheet.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="section">
+    <div class="container">
+      <h1 class="title">{{character.name}}</h1>
+
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Character } from '@/models/character.ts';
+
+@Component
+export default class CharacterSheet extends Vue {
+  data() {
+    return { character: getCharacterData() };
+  }
+}
+
+function getCharacterData(): Character {
+  return new Character('Balthasar the god killer', 1, 'Aaron');
+}
+</script>
---
+++ src/models/character.ts
@@ -0,0 +1,10 @@
+export class Character {
+  constructor(name : string, level: number, playerName: string) {
+    this.level = level;
+    this.name = name;
+    this.playerName = playerName;
+  }
+    public name = '';
+    public level = 1;
+    public playerName = '';
+}
